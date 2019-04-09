4ceaabf849f984fb170d15935b24ba60cb6af25e
IhorShylov <33127995+maximusigor@users.noreply.github.com>
2019-04-09 22:51:01 +0300

    Add files via upload

---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img src='https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500' />
+        </div>
+        <div>
+            ava + description
+    </div>
+        <div>
+            My post
+        <div>
+                New post
+        </div>
+        </div>
+        <div>
+            <div>
+                post1
+        </div>
+            <div>
+                post2
+        </div>
+        </div>
+    </div>
+}
+
+export default Profile;
