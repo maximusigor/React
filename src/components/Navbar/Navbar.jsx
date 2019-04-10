39f9e3234573ffa58af4d43b1846d65fbc52c315
Игорь <shilov0209@gmail.com>
2019-04-11 01:43:21 +0300

    added new packajes and changed files

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import s from './Navbar.module.css';
-
+console.log(s);
 // let s = {
 //     'nav' : 'Navbar_nav__2WnEw',
 //     'item' : 'Navbar_item__3r99d'
+//     'active' : 'Navbar_active__3r23f'
 // }
+
+// let c1 = "item";
+// let c2 = "active";
+// "item active"
+// let classes = c1 + " " + c2;
+// let classesNew = `${s.item} ${s.active}`;
 
 const Navbar = () => {
     return <nav className={s.nav}>
         <div className={s.item}>
             <a>Profile</a>
         </div>
-        <div className={s.item}>
+        <div className={`${s.item} ${s.active}`}>
             <a>Messages</a>
         </div>
         <div className={s.item}>
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,29 +1,19 @@
 import React from 'react';
 import s from './Profile.module.css';
+import MyPosts from './MyPosts/MyPosts';
 
 const Profile = () => {
-    return <div className={s.content}>
-        <div>
-            <img src='https://tproger.ru/wp-content/uploads/2017/05/react-app.jpg'/>
-        </div>
-        <div>
-            ava + description
-    </div>
-        <div>
-            My post
-        <div>
-            New post
-        </div>
-        </div>
-        <div className={s.item}>
-            <div className={s.item}>
-                post1
+    return (
+        <div className={s.content}>
+            <div>
+                <img src='https://tproger.ru/wp-content/uploads/2017/05/react-app.jpg' />
             </div>
-            <div className={s.item}>
-                post2
+            <div>
+                ava + description
             </div>
-        </div>
-    </div>
+                <MyPosts />
+            </div>
+    )
 }
 
 export default Profile;
