09cc35166e149dc7073dc1a4251e07de388bb606
Игорь <shilov0209@gmail.com>
2019-04-20 01:28:59 +0300

    Changed files

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -20,20 +20,38 @@ const Message = (props) => {
 }
 
 const Dialogs = (props) => {
+
+    let dialogsData = [
+        {id:1, name:'Igor'},
+        {id:2, name:'Artem'},
+        {id:3, name:'Natalya'},
+        {id:4, name:'Viktor'},
+        {id:5, name:'Sergey'},
+        {id:6, name:'Dimych'}
+    ]
+
+    let messagesData = [
+        {id:1, message:'Hi'},
+        {id:2, message:'How are you?'},
+        {id:3, message:'Yo'},
+        {id:4, message:'React'},
+        {id:5, message:'Hello World'},
+        {id:6, message:'I am front-end developer'}
+    ]
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <DialogItem name="Igor" id="1" />
-                <DialogItem name="Artem" id="2" />
-                <DialogItem name="Natalya" id="3" />
-                <DialogItem name="Viktor" id="4" />
-                <DialogItem name="Sergey" id="5" />
-                <DialogItem name="Dimych" id="6" />
+                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
+                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
             </div>
             <div className={s.messages}>
-                <Message message="Hi"/>
-                <Message message="How are you?"/>
-                <Message message="Yo"/>
+                <Message message={messagesData[0].message} />
+                <Message message={messagesData[1].message} />
+                <Message message={messagesData[2].message} />
+                <Message message={messagesData[3].message} />
+                <Message message={messagesData[4].message} />
+                <Message message={messagesData[5].message} />
             </div>
         </div>
     )
