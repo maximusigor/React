499b1aea3b193cba63603f47b087eec39e008495
Игорь <shilov0209@gmail.com>
2019-04-19 18:14:59 +0300

    add links for dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,28 +3,29 @@
  */
 import React from 'react';
 import s from './Dialogs.module.css';
+import {NavLink} from 'react-router-dom';
 
 const Dialogs = (props) => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
                 <div className={s.dialog + ' ' + s.active}>
-                    Igor
+                    <NavLink to="/dialogs/1">Igor</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Artem
+                    <NavLink to="/dialogs/2">Artem</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Natalya
+                    <NavLink to="/dialogs/3">Natalya</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Viktor
+                    <NavLink to="/dialogs/4">Viktor</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Sergey
+                    <NavLink to="/dialogs/5">Sergey</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Dimych
+                    <NavLink to="/dialogs/6">Dimych</NavLink>
                 </div>
             </div>
             <div className={s.messages}>
@@ -34,6 +35,5 @@ const Dialogs = (props) => {
             </div>
         </div>
     )
-
 }
 export default Dialogs;
