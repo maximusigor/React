0da0f68a93c564714baa9cf27947f4a36c6035e5
Игорь <shilov0209@gmail.com>
2019-04-24 03:13:39 +0300

    create file state.js and variables

--- src/index.js
+++ src/index.js
@@ -3,32 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import state from './redux/state';
 
-let dialogs = [
-    {id:1, name:'Igor'},
-    {id:2, name:'Artem'},
-    {id:3, name:'Natalya'},
-    {id:4, name:'Viktor'},
-    {id:5, name:'Sergey'},
-    {id:6, name:'Dimych'}
-];
-
-let messages = [
-    {id:1, message:'Hi'},
-    {id:2, message:'How are you?'},
-    {id:3, message:'Yo'},
-    {id:4, message:'React'},
-    {id:5, message:'Hello World'},
-    {id:6, message:'I am front-end developer'}
-
-];
-
-let posts = [
-    {id: 1, message: 'Hi, React! I am a new user', likesCount: 1},
-    {id: 2, message: '-Hi, user! Welcome in my world!', likesCount: 23}
-];
-
-ReactDOM.render(<App posts={posts} dialogs={dialogs} messages={messages} />, document.getElementById('root'));
+ReactDOM.render(<App state={state} />, document.getElementById('root'));
 
 
 // If you want your app to work offline and load faster, you can change
---
+++ src/redux/state.js
@@ -0,0 +1,34 @@
+/**
+ * Created by Игорь on 24.04.2019.
+ */
+
+let state ={
+    profilePage: {
+        posts : [
+            {id: 1, message: 'Hi, React! I am a new user', likesCount: 1},
+            {id: 2, message: '-Hi, user! Welcome in my world!', likesCount: 23}
+        ]
+    },
+
+    dialogsPage : {
+        messages: [
+            {id:1, message:'Hi'},
+            {id:2, message:'How are you?'},
+            {id:3, message:'Yo'},
+            {id:4, message:'React'},
+            {id:5, message:'Hello World'},
+            {id:6, message:'I am front-end developer'}
+
+        ],
+        dialogs: [
+            {id:1, name:'Igor'},
+            {id:2, name:'Artem'},
+            {id:3, name:'Natalya'},
+            {id:4, name:'Viktor'},
+            {id:5, name:'Sergey'},
+            {id:6, name:'Dimych'}
+        ]
+    }
+};
+
+export default state;
